8d62c36dd40e84acbed0e192e26dd96f7a64cf8b
Tasniem Fataloun <tasniem.fataloun@student.arteveldehs.be>
2023-06-03 12:12:59 +0200

    eindresultaat

--- Eindopdracht-moweb 2/js/tasneemfataloun.js
+++ Eindopdracht-moweb 2/js/tasneemfataloun.js
@@ -12,7 +12,7 @@ const moveCursor = (e)=> {
     let deg = Math.atan2(mouseY - cursorY, mouseX - cursorX) * 180 / Math.PI;
 
     if(mouseX < cursorX) {
-        deg = deg-180; //andere richting draaien
+        deg = deg-180; 
     } 
     scale = Math.floor(1 + dist/200);
     
@@ -24,11 +24,6 @@ const moveCursor = (e)=> {
         scale: scale,
     });
 }
-
-
-/* const main = document.querySelector("main");
-
-let text1 = new SplitType('#textPlanet1', { types: 'words, chars' })  */
 
 gsap.registerPlugin(ScrollTrigger);
   
@@ -70,20 +65,10 @@ function tasneemfataloun(action) {
             .to("#tasneemfataloun .redSun span.redPlanet", {
                 opacity: 1,
                 borderRadius: "50%", 
-                //duration: 3,  
             }, "makeGlobe")
-/*             .from(text1.chars, {
-                yPercent: 150,
-                stagger: 0.05,
-                ease: "back.out",
-                duration: 1}) */
             .to("#tasneemfataloun .redSun", {
                 borderRadius: "50%", 
-                //duration: 3,
-            }, "makeGlobe")
-/*             .from(text1.chars, {  opacity: 0,  y: 30,  duration: 0.5,  stagger: { amount: 1},})
- */            
-             
+            }, "makeGlobe")        
             .to("#tasneemfataloun .redSun span.homePlanet", {
                 borderRadius: "50%",
                 opacity: 0,
@@ -91,63 +76,52 @@ function tasneemfataloun(action) {
 
         .add("bigSun")
             .to("#tasneemfataloun .redSun", {
-                //duration: 10,
                 width: "30rem", 
                 height: "30rem",
                 top: "30rem",
                 left: "5rem",
                 zIndex: -10, 
             }, "bigSun")
-            /*
-            .to("#tasneemfataloun .redSun", {
-                //duration: 30,
-            }, "bigSun")
-            */
 
-            .add("SizeSun2")
-                .to("#tasneemfataloun .redSun", {
-                    //duration: 10, 
-                    borderRadius: "0",
-                    top: 0,
-                    width: "100%",
-                    height: "100%",
-                    left: 0,
-                    borderRadius: "0",
-                }, "SizeSun2")
-                .to("#tasneemfataloun .redSun span.redPlanet", {
-                    opacity: 0,
-                    borderRadius: "0", 
-                }, "SizeSun2")
-                .to("#tasneemfataloun .redSun span.homePlanet", {
-                    borderRadius: "0",
-                    opacity: 1,
-                }, "SizeSun2") 
+        .add("SizeSun2")
+            .to("#tasneemfataloun .redSun", {
+                borderRadius: "0",
+                top: 0,
+                width: "100%",
+                height: "100%",
+                left: 0,
+                borderRadius: "0",
+            }, "SizeSun2")
+            .to("#tasneemfataloun .redSun span.redPlanet", {
+                opacity: 0,
+                borderRadius: "0", 
+            }, "SizeSun2")
+            .to("#tasneemfataloun .redSun span.homePlanet", {
+                borderRadius: "0",
+                opacity: 1,
+            }, "SizeSun2") 
         
 
             .add("dalik1")
                 .to("#tasneemfataloun .dalik1", { 
                     opacity: 1, 
                     left: "10rem", 
-                    //duration: 5, 
                 }, "dalik1") 
 
             .add("dalik2")
                 .to("#tasneemfataloun .dalik2", { 
                     opacity: 1, 
                     right: "10rem", 
-                    //duration: 5, 
                 }, "dalik2") 
 
             .add("moveDalik")
                 .to("#tasneemfataloun .dalik1", { 
                     opacity: 1, 
                     left: "100rem", 
-                    //duration: 5, 
                 }, "moveDalik") 
                 .to("#tasneemfataloun .dalik2", { 
                     opacity: 1, 
                     right: "100rem", 
-                    //duration: 5, 
                 }, "moveDalik") 
 
             .add("police")
@@ -155,7 +129,6 @@ function tasneemfataloun(action) {
                     opacity: 1, 
                     left: "110vw",   
                     rotation: 360,
-                    //duration: 5, 
                     opacity: 1, 
                     top: "5vh",
                     left: "45vw",
@@ -163,35 +136,35 @@ function tasneemfataloun(action) {
 
             .add("landing")
                 .to("#tasneemfataloun .policeBox", { 
-                    //duration: 5, 
                     top: "35vh",
                 }, "landing") 
 
             .add("changeBackground")
-            .to("#tasneemfataloun .homePlanet", { 
+                .to("#tasneemfataloun .homePlanet", { 
                 backgroundImage: "url(tasneemfataloun/Orphan.webp)", 
-            }, "changeBackground") 
-            .add("changeBackground2")
-            .to("#tasneemfataloun .homePlanet", { 
-                backgroundImage: "url(tasneemfataloun/tardis.webp)", 
-            }, "changeBackground2") 
+                }, "changeBackground") 
 
-        .add("reset")
-        .to("#tasneemfataloun .policeBox", { 
-            opacity: 0, 
-        }, "reset")
-        .to("#tasneemfataloun .homePlanet", { 
-            opacity: 0, 
-        }, "reset") 
-        .to("#tasneemfataloun .redSun", { 
-            width: "20vw", 
-            height: "20vw", 
-            left: "40vw", 
-            top: "calc(50vh - 10vw)", 
-            borderRadius: "0", 
-            //duration:30, 
-            rotation: 360,
-        }, "reset")
+            .add("changeBackground2")
+                .to("#tasneemfataloun .homePlanet", { 
+                backgroundImage: "url(tasneemfataloun/tardis.webp)", 
+                }, "changeBackground2") 
+
+            .add("reset")
+                .to("#tasneemfataloun .policeBox", { 
+                    opacity: 0, 
+                }, "reset")
+                .to("#tasneemfataloun .homePlanet", { 
+                    opacity: 0, 
+                }, "reset") 
+                .to("#tasneemfataloun .redSun", { 
+                    width: "20vw", 
+                    height: "20vw", 
+                    left: "40vw", 
+                    top: "calc(50vh - 10vw)", 
+                    borderRadius: "0", 
+                    //duration:30, 
+                    rotation: 360,
+                }, "reset")
         
     }
 }
